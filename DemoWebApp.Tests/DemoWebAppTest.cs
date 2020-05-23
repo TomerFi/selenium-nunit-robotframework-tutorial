995855d0311f5888eba883f5e5706eb6b00bad11
Tomer Figenblat <tomer.figenblat@gmail.com>
2020-05-23 17:56:08 +0300

    Initial commit.

---
+++ DemoWebApp.Tests/DemoWebAppTest.cs
@@ -0,0 +1,50 @@
+
+using System;
+using NUnit.Framework;
+using Microsoft.Extensions.Hosting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace DemoWebApp.Tests
+{
+    [TestFixture]
+    public class DemoWebAppTest
+    {
+        private IHost app;
+
+        [OneTimeSetUp]
+        public void SetUpWebApp()
+        {
+            app = DemoWebApp.Program.CreateHostBuilder(new string[] {}).Build();
+            app.RunAsync();
+        }
+
+        [TestCase(typeof(ChromeDriver))]
+        [TestCase(typeof(FirefoxDriver))]
+        [TestCase(typeof(InternetExplorerDriver))] // ie requires workstation configuration: http://www.programmersought.com/article/1603471677/
+        public void TestButtonClick(Type drvType)
+        {
+            bool clicked;
+            using (var driver = (IWebDriver) Activator.CreateInstance(drvType))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl("http://localhost:5000");
+                driver.FindElement(By.Id("clickmeButton")).Click();
+
+                clicked = wait.Until(ExpectedConditions.TextToBePresentInElement(
+                  driver.FindElement(By.Id("displayHeader")), "Button clicked"));
+            }
+            Assert.True(clicked, "button not clicked.");
+        }
+
+        [OneTimeTearDown]
+        public void TearDownWebApp()
+        {
+            app.StopAsync();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Example C# WebApp Testing with Nunit and Selenium
+
+**Build dotnet core > 3.**
+
+Example of a simple c# WebApp (created from the basic template).</br>
+The base WebApp was added with a button, an h2 header and a script that change the h2 text content when the button is clicked.
+
+In the testing area, we have a test fixture starting our app at `localhost:5000` and stopping it when the tests are done,</br>
+And a test made from a couple of test cases (one for each browser).</br>
+
+The test uses selenium to browse our app, click the button and validate the text has changed.
+
+To check it out, just clone it and run the tests:
+
+```shell
+dotnet test
+```
+
+## Literature
+
+- [Nunit3 home](https://nunit.org/)
+- [Nunit3 docs](https://github.com/nunit/docs/wiki)
+- [Selenium home](https://www.selenium.dev/)
+- [Selenium docs](https://www.selenium.dev/documentation/en/)
+
+> Please Note: the repository name implies the use of [Robot Framework](https://robotframework.org).</br>
+> Hopefully a usage example of [Robot Framework](https://robotframework.org) as an automation platform will be added shortly.
